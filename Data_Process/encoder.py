455b06a5e418b28d98e31702281c6a8bee52c3bd
jambobjambo <jamie.a.mcinally@gmail.com>
2017-03-01 11:49:38 +0000

    Neural nets setup for all entities

--- Data_Process/encoder.py
+++ Data_Process/encoder.py
@@ -107,6 +107,26 @@ def gen_M_vec(line, array):
 			vec_amount[rep_index_value] += 1
 	return vec_amount
 
+def gen_L_vec(line, word):
+	line_split = line.split(" ")
+
+	len_row = len(line_split)
+	length_of_row = 15
+	vec = np.zeros(length_of_row)
+	if len_row < length_of_row:
+		left_over = length_of_row - len_row
+
+	for steps_in_words in range(length_of_row):
+		if steps_in_words < len(line_split):
+			if line_split[steps_in_words].lower() == word.lower():
+				vec[steps_in_words] += 1
+	return vec
+
+def gen_L_vec_none():
+	length_of_row = 15
+	vec = np.zeros(length_of_row)
+	return vec
+
 def gen_vecs(lexicon, intents, recipes, categories, calendar, test_size=0.1):
 	#Loop till have required lines stored
 	Total_lines = Desired_Data_Amount
@@ -129,22 +149,32 @@ def gen_vecs(lexicon, intents, recipes, categories, calendar, test_size=0.1):
 		training_data.append([])
 		training_data.append([])
 		training_data.append([])
+		training_data.append([])
+		training_data.append([])
+		training_data.append([])
+		training_data.append([])
 
 		for lines in lines_arrays:
 			query = gen_query_vec(lines[0], lexicon)
 			int = gen_intent_vec(lines[1], intents)
 			rec = gen_M_vec(lines[2], recipes)
 			cat = gen_M_vec(lines[3], categories)
-			#Vec for attributes
 			cal = gen_M_vec(lines[5], calendar)
-			#Vec for Places
-			#Vec for Grouping
-			#Vec for filter
+			attr = gen_L_vec(lines[0], lines[4])
+			plc = gen_L_vec(lines[0], lines[6])
+			if len(lines) > 7:
+				grp = gen_L_vec(lines[0], lines[7])
+			else:
+				grp = gen_L_vec_none()
 			training_data[0].append(query)
 			training_data[1].append(int)
 			training_data[2].append(rec)
 			training_data[3].append(cat)
 			training_data[4].append(cal)
+			training_data[5].append(attr)
+			training_data[6].append(plc)
+			training_data[7].append(grp)
+
 
 		#Save training data batch in pickle
 		training_data = np.array(training_data)
@@ -156,16 +186,35 @@ def gen_vecs(lexicon, intents, recipes, categories, calendar, test_size=0.1):
 		train_recipe = list(training_data[2][:-testing_size])
 		train_categories = list(training_data[3][:-testing_size])
 		train_calendar = list(training_data[4][:-testing_size])
+		train_attributes = list(training_data[5][:-testing_size])
+		train_places = list(training_data[6][:-testing_size])
+		train_groups = list(training_data[7][:-testing_size])
 		#Testing data define
-		test_query = list(training_data[0][:-testing_size])
-		test_intent = list(training_data[1][:-testing_size])
-		test_recipe = list(training_data[2][:-testing_size])
-		test_categories = list(training_data[3][:-testing_size])
-		test_calendar = list(training_data[4][:-testing_size])
+		test_query = list(training_data[0][-testing_size:])
+		test_intent = list(training_data[1][-testing_size:])
+		test_recipe = list(training_data[2][-testing_size:])
+		test_categories = list(training_data[3][-testing_size:])
+		test_calendar = list(training_data[4][-testing_size:])
+		test_attributes = list(training_data[5][-testing_size:])
+		test_places = list(training_data[6][-testing_size:])
+		test_groups = list(training_data[7][-testing_size:])
 
-		with open( Pickle_location + '/' + str(file) + '.pickle', 'wb') as f:
-			pickle.dump([train_query, train_intent, train_recipe, train_categories, train_calendar, test_query, test_intent, test_recipe, test_categories, test_calendar], f, protocol=2)
-
+		with open( Pickle_location + '/Queries/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_query, test_query], f, protocol=2)
+		with open( Pickle_location + '/Intent/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_intent, test_intent], f, protocol=2)
+		with open( Pickle_location + '/Recipe/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_recipe, test_recipe], f, protocol=2)
+		with open( Pickle_location + '/Category/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_categories, test_categories], f, protocol=2)
+		with open( Pickle_location + '/Calendar/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_calendar, test_calendar], f, protocol=2)
+		with open( Pickle_location + '/Attributes/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_attributes, test_attributes], f, protocol=2)
+		with open( Pickle_location + '/Places/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_places, test_places], f, protocol=2)
+		with open( Pickle_location + '/Grouping/' + str(file) + '.pickle', 'wb') as f:
+			pickle.dump([train_groups, test_groups], f, protocol=2)
 		file += 1
 
 
---
+++ Entities/train_attributes.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+import os
+
+pickle_directory = '../Data_Process/DataForML/'
+
+train_q, test_q = pickle.load(open(pickle_directory + '/Queries/' + '0.pickle', 'rb'))
+train_atr, test_atr = pickle.load(open(pickle_directory + '/Attributes/' + '0.pickle', 'rb'))
+
+n_nodes_hl1 = 1500
+n_nodes_hl2 = 1500
+n_nodes_hl3 = 1500
+
+n_classes = len(train_atr[0])
+batch_size = 100
+hm_epochs = 10
+
+x = tf.placeholder('float')
+y = tf.placeholder('float')
+
+hidden_1_layer = {'f_fum':n_nodes_hl1,
+                  'weight':tf.Variable(tf.random_normal([len(train_q[0]), n_nodes_hl1])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+hidden_2_layer = {'f_fum':n_nodes_hl2,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+hidden_3_layer = {'f_fum':n_nodes_hl3,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+output_layer = {'f_fum':None,
+                'weight':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                'bias':tf.Variable(tf.random_normal([n_classes])),}
+
+
+# Nothing changes
+def neural_network_model(data):
+	l1 = tf.add(tf.matmul(data,hidden_1_layer['weight']), hidden_1_layer['bias'])
+	l1 = tf.nn.relu(l1)
+
+	l2 = tf.add(tf.matmul(l1,hidden_2_layer['weight']), hidden_2_layer['bias'])
+	l2 = tf.nn.relu(l2)
+
+	l3 = tf.add(tf.matmul(l2,hidden_3_layer['weight']), hidden_3_layer['bias'])
+	l3 = tf.nn.relu(l3)
+
+	output = tf.matmul(l3,output_layer['weight']) + output_layer['bias']
+
+	return output
+
+def train_neural_network(x):
+	prediction = neural_network_model(x)
+	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+	optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			files = os.listdir(pickle_directory + '/Queries') # dir is your directory path
+			file_num = 0
+			for file in files:
+				file_num += 1
+				train_query, test_query = pickle.load(open(pickle_directory + '/Queries/' + file, 'rb'))
+				train_attributes, test_attributes = pickle.load(open(pickle_directory + '/Attributes/' + file, 'rb'))
+				i=0
+				while i < len(train_query):
+					start = i
+					end = i+batch_size
+					batch_x = np.array(train_query[start:end])
+					batch_y = np.array(train_attributes[start:end])
+
+					_, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+					                                              y: batch_y})
+					epoch_loss += c
+					i+=batch_size
+				print("File: " + str(file_num) + " out of " + str(len(files)) + " trained")
+			correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+			accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+			print('Epoch', epoch+1, 'completed out of',hm_epochs,'Loss:',epoch_loss,"Accuracy:", accuracy.eval({x: test_q, y: test_atr}))
+
+
+
+train_neural_network(x)
